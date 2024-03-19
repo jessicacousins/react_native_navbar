415603cd6388ab45c5e4059cd3467b00cc4ba6e1
Jessica Cousins <113946660+jessicacousins@users.noreply.github.com>
2024-03-18 21:29:00 -0400

    start

--- App.js
+++ App.js
@@ -1,20 +1,115 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text, Button, Image, Alert } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFocusEffect } from "@react-navigation/native";
 
-export default function App() {
+function LogoTitle() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require("./assets/humanIcon1.png")}
+    />
+  );
+}
+
+function HomeScreen({ navigation }) {
+  React.useEffect(() => {
+    navigation.setOptions({
+      title: "My Home",
+      headerStyle: {
+        backgroundColor: "#f4511e",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "bold",
+      },
+      headerTitle: (props) => <LogoTitle {...props} />,
+      headerRight: () => (
+        <Button
+          onPress={() => Alert.alert("This is a button!")}
+          title="Info"
+          color="#fff"
+        />
+      ),
+    });
+  }, [navigation]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      Alert.alert(`Home Screen is focused`);
+      return () => {
+        Alert.alert(`Leaving Home Screen `);
+      };
+    }, [])
+  );
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          navigation.navigate("Details", {
+            itemId: 86,
+            otherParam: "anything you want here",
+          });
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ route, navigation }) {
+  const { itemId, otherParam } = route.params;
+
+  React.useEffect(() => {
+    navigation.setOptions({
+      title: `Details for item ${itemId}`,
+      headerBackTitle: "Custom Back",
+      headerBackTitleStyle: { fontSize: 30 },
+    });
+  }, [navigation, itemId]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      Alert.alert(`Details Screen is focused`);
+      return () => {
+        Alert.alert(`Leaving Current Screen `);
+      };
+    }, [itemId])
+  );
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push("Details", {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,24 @@
+# NavBar Demo 101
+
+- `npx create-expo-app NavBar101`
+- `npx expo install react-dom react-native-web @expo/metro-runtime`
+- `npm install @react-navigation/native`
+- `npx expo install react-native-screens react-native-safe-area-context`
+- `npm install @react-navigation/native-stack`
+
+#
+
+### start:
+
+```
+npx expo start
+```
+
+#
+
+### Tutorial from:
+
+```
+https://reactnavigation.org/docs/getting-started
+
+```
